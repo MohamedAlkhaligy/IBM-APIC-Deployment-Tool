d8b2a5f0c5a1c17f1e4b8e20315a1268769b8bfa
Mohamed <mohamedh.alkhaligy@gmail.com>
2023-04-01 18:25:39 +0200

    Add x-ibm-configuration to model

--- lib/models/products/openapi.dart
+++ lib/models/products/openapi.dart
@@ -6,9 +6,12 @@ part 'openapi.g.dart';
 class OpenAPI {
   @JsonKey(name: 'info')
   final APIInfo info;
+  @JsonKey(name: 'x-ibm-configuration')
+  final IbmConfiguration ibmConfiguration;
 
   OpenAPI({
     required this.info,
+    required this.ibmConfiguration,
   });
 
   factory OpenAPI.fromJson(Map<String, dynamic> json) =>
@@ -37,4 +40,29 @@ class APIInfo {
   factory APIInfo.fromJson(Map<String, dynamic> json) =>
       _$APIInfoFromJson(json);
   Map<String, dynamic> toJson() => _$APIInfoToJson(this);
+}
+
+@JsonSerializable()
+class IbmConfiguration {
+  final APIAssembly assembly;
+
+  IbmConfiguration({required this.assembly});
+
+  factory IbmConfiguration.fromJson(Map<String, dynamic> json) =>
+      _$IbmConfigurationFromJson(json);
+  Map<String, dynamic> toJson() => _$IbmConfigurationToJson(this);
+}
+
+@JsonSerializable()
+class APIAssembly {
+  @JsonKey(name: 'execute')
+  final List<dynamic>? executeList;
+  @JsonKey(name: 'catch')
+  final List<dynamic>? catchList;
+
+  APIAssembly({required this.executeList, required this.catchList});
+
+  factory APIAssembly.fromJson(Map<String, dynamic> json) =>
+      _$APIAssemblyFromJson(json);
+  Map<String, dynamic> toJson() => _$APIAssemblyToJson(this);
 }
--- lib/models/products/openapi.g.dart
+++ lib/models/products/openapi.g.dart
@@ -8,10 +8,13 @@ part of 'openapi.dart';
 
 OpenAPI _$OpenAPIFromJson(Map<String, dynamic> json) => OpenAPI(
       info: APIInfo.fromJson(json['info'] as Map<String, dynamic>),
+      ibmConfiguration: IbmConfiguration.fromJson(
+          json['x-ibm-configuration'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$OpenAPIToJson(OpenAPI instance) => <String, dynamic>{
       'info': instance.info,
+      'x-ibm-configuration': instance.ibmConfiguration,
     };
 
 APIInfo _$APIInfoFromJson(Map<String, dynamic> json) => APIInfo(
@@ -24,4 +27,25 @@ Map<String, dynamic> _$APIInfoToJson(APIInfo instance) => <String, dynamic>{
       'version': instance.version,
       'title': instance.title,
       'x-ibm-name': instance.name,
+    };
+
+IbmConfiguration _$IbmConfigurationFromJson(Map<String, dynamic> json) =>
+    IbmConfiguration(
+      assembly: APIAssembly.fromJson(json['assembly'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$IbmConfigurationToJson(IbmConfiguration instance) =>
+    <String, dynamic>{
+      'assembly': instance.assembly,
+    };
+
+APIAssembly _$APIAssemblyFromJson(Map<String, dynamic> json) => APIAssembly(
+      executeList: json['execute'] as List<dynamic>?,
+      catchList: json['catch'] as List<dynamic>?,
+    );
+
+Map<String, dynamic> _$APIAssemblyToJson(APIAssembly instance) =>
+    <String, dynamic>{
+      'execute': instance.executeList,
+      'catch': instance.catchList,
     };
