93e78e30b1044fb3a551ce2d8484e160de6a8519
Mohamed <mohamedh.alkhaligy@gmail.com>
2023-06-02 01:08:08 +0200

    Add Plan APIs

--- lib/models/products/product.dart
+++ lib/models/products/product.dart
@@ -78,10 +78,10 @@ class Info {
 @JsonSerializable(explicitToJson: true)
 class PlanDetails {
   @JsonKey(name: 'rate-limits')
-  final Map<String, Details> rateLimits;
+  final Map<String, LimitDetails> rateLimits;
 
   @JsonKey(name: 'burst-limits', includeIfNull: false)
-  final Map<String, Details>? burstLimits;
+  final Map<String, LimitDetails>? burstLimits;
 
   @JsonKey(name: 'title', includeIfNull: false)
   final String? title;
@@ -92,12 +92,16 @@ class PlanDetails {
   @JsonKey(name: 'approval', includeIfNull: false)
   final bool? approval;
 
+  @JsonKey(name: 'apis', includeIfNull: false)
+  final Map<String, PlanAPIs>? apis;
+
   PlanDetails({
     required this.rateLimits,
     this.burstLimits,
     required this.title,
     required this.description,
     required this.approval,
+    this.apis,
   });
 
   factory PlanDetails.fromJson(Map<String, dynamic> json) =>
@@ -106,18 +110,45 @@ class PlanDetails {
 }
 
 @JsonSerializable()
-class Details {
+class PlanAPIs {
+  @JsonKey(name: 'operations', includeIfNull: false)
+  List<Operation>? operations;
+
+  PlanAPIs({this.operations});
+
+  factory PlanAPIs.fromJson(Map<String, dynamic> json) =>
+      _$PlanAPIsFromJson(json);
+  Map<String, dynamic> toJson() => _$PlanAPIsToJson(this);
+}
+
+@JsonSerializable()
+class Operation {
+  @JsonKey(name: 'operation')
+  String operation;
+
+  @JsonKey(name: 'path')
+  String path;
+
+  Operation({required this.operation, required this.path});
+
+  factory Operation.fromJson(Map<String, dynamic> json) =>
+      _$OperationFromJson(json);
+  Map<String, dynamic> toJson() => _$OperationToJson(this);
+}
+
+@JsonSerializable()
+class LimitDetails {
   @JsonKey(name: 'value')
   final String value;
 
   @JsonKey(name: 'hard-limit', includeIfNull: false)
   final bool? hardLimit;
 
-  Details({required this.value, this.hardLimit});
+  LimitDetails({required this.value, this.hardLimit});
 
-  factory Details.fromJson(Map<String, dynamic> json) =>
-      _$DetailsFromJson(json);
-  Map<String, dynamic> toJson() => _$DetailsToJson(this);
+  factory LimitDetails.fromJson(Map<String, dynamic> json) =>
+      _$LimitDetailsFromJson(json);
+  Map<String, dynamic> toJson() => _$LimitDetailsToJson(this);
 }
 
 @JsonSerializable()
--- lib/models/products/product.g.dart
+++ lib/models/products/product.g.dart
@@ -54,14 +54,17 @@ Map<String, dynamic> _$InfoToJson(Info instance) {
 
 PlanDetails _$PlanDetailsFromJson(Map<String, dynamic> json) => PlanDetails(
       rateLimits: (json['rate-limits'] as Map<String, dynamic>).map(
-        (k, e) => MapEntry(k, Details.fromJson(e as Map<String, dynamic>)),
+        (k, e) => MapEntry(k, LimitDetails.fromJson(e as Map<String, dynamic>)),
       ),
       burstLimits: (json['burst-limits'] as Map<String, dynamic>?)?.map(
-        (k, e) => MapEntry(k, Details.fromJson(e as Map<String, dynamic>)),
+        (k, e) => MapEntry(k, LimitDetails.fromJson(e as Map<String, dynamic>)),
       ),
       title: json['title'] as String?,
       description: json['description'] as String?,
       approval: json['approval'] as bool?,
+      apis: (json['apis'] as Map<String, dynamic>?)?.map(
+        (k, e) => MapEntry(k, PlanAPIs.fromJson(e as Map<String, dynamic>)),
+      ),
     );
 
 Map<String, dynamic> _$PlanDetailsToJson(PlanDetails instance) {
@@ -80,15 +83,45 @@ Map<String, dynamic> _$PlanDetailsToJson(PlanDetails instance) {
   writeNotNull('title', instance.title);
   writeNotNull('description', instance.description);
   writeNotNull('approval', instance.approval);
+  writeNotNull('apis', instance.apis?.map((k, e) => MapEntry(k, e.toJson())));
   return val;
 }
 
-Details _$DetailsFromJson(Map<String, dynamic> json) => Details(
+PlanAPIs _$PlanAPIsFromJson(Map<String, dynamic> json) => PlanAPIs(
+      operations: (json['operations'] as List<dynamic>?)
+          ?.map((e) => Operation.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$PlanAPIsToJson(PlanAPIs instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('operations', instance.operations);
+  return val;
+}
+
+Operation _$OperationFromJson(Map<String, dynamic> json) => Operation(
+      operation: json['operation'] as String,
+      path: json['path'] as String,
+    );
+
+Map<String, dynamic> _$OperationToJson(Operation instance) => <String, dynamic>{
+      'operation': instance.operation,
+      'path': instance.path,
+    };
+
+LimitDetails _$LimitDetailsFromJson(Map<String, dynamic> json) => LimitDetails(
       value: json['value'] as String,
       hardLimit: json['hard-limit'] as bool?,
     );
 
-Map<String, dynamic> _$DetailsToJson(Details instance) {
+Map<String, dynamic> _$LimitDetailsToJson(LimitDetails instance) {
   final val = <String, dynamic>{
     'value': instance.value,
   };
